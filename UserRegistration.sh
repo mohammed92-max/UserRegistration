d5f579a4592ddedd823117affb46771ce6ec25e2
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-11 11:30:28 +0530

    pushed UseCase2

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,8 +3,8 @@
 #pattern="^([1-9][0-9]{5})$"
 #input="123456"
 
-echo"first name"
-pattern="^([A-Z][a-z]*)$"
+echo "Last name problem"
+pattern="^([A-Z][a-z]{2,})$"
 input="Shah"
 if [[ $input =~ $pattern ]];
 then echo true;
