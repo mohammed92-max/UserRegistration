8037ce33f08930c19cc7df31fb868700ea5d27c0
mohammed92-max <shahrukhkhandb@gmail.com>
2020-05-13 17:57:50 +0530

    added sh file

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,26 +1,30 @@
 #!/bin/bash -x
 
-#pattern="([1-9]{1,}[a-z][\]{1}[A-Z]{1,}*)$"
-
-#pattern="^([0-9 a-z][A-Z{1,}]*){8,}$"
-#pattern="([1-9]{1,}[a-z][A-Z]{1,}*)$"
-#pattern="^([0-9 a-z A-Z]){8,})$"
-
-
-#all rules pattern some what close.
-#pattern="^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
-
-
-
-#UC5
-#pattern="^([0-9 a-z A-Z]{8,})$"
-
-#UC6 case
-pattern="(?=.*[A-Z])([A-Za-z\d@$!%*?&]){8,}$"
-input="Shahrukh"
+pattern="^([a-z][a-z 0-9 \ ]*[@][a-z]*[.][a-z . ]*)$"
+input="abc@yahoo.com"
+input="abc-100@yahoo.com"
+input="abc.100@yahoo.com"
+input="abc111@abc.com"
+input="abc-100@abc.net"
+input="abc.100@abc.com.au"
+input="abc@1.com"
+input="abc@gmail.com.com"
+input="abc+100@gmail.com"
+input="abc"
+input="abc@.com"
+input="abc123@gmail.a"
+input="abc123@.com"
+input="abc123@.com.com"
+input="abc@abc.com"
+input="abc()*@gmail.com"
+input="abc@%*.com"
+input="abc..2010@gmail.com"
+input="abc@gmail.com.1a"
+input="abc.@gmail.com"
+input="abc@abc@gmail.com"
+input="abc@gmail.com.aa.au"
 
 if [[ $input =~ $pattern ]];
 then echo true;
 else echo false;
 fi
-
